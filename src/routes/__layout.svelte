fcd3517a021e106915fb5a52b90b36747cfcd310
Timothy Foster <tf.auroratide@gmail.com>
2022-01-05 14:16:47 -0600

    Start styling homepage

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,11 @@
+<slot></slot>
+
+<style global>
+    :root {
+        --font: 'Kanit', -apple-system, 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Noto Sans', 'Ubuntu', 'Droid Sans', 'Helvetica Neue', sans-serif;
+    }
+
+    body {
+        font-family: var(--font);
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <title>Sifetti</title>
+</svelte:head>
+
+<header class="banner">
+    <div class="container">
+        <h1 class="title">Sifetti</h1>
+        <section class="please-join">
+            <p>Perfect document organization with zero folders.</p>
+            <a href="/sign-in">Sign In</a>
+        </section>
+    </div>
+</header>
+
+<style lang="scss">
+    .banner {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background: hsl(210, 68%, 45%);
+        min-height: 90vh;
+        clip-path: polygon(0% 0%, 100% 0%, 100% 100%, 0% 90%);
+
+        .container {
+            max-width: 75rem;
+            display: flex;
+            flex-wrap: wrap;
+            align-items: center;
+            justify-content: center;
+        }
+    }
+
+    .title {
+        display: inline-block;
+        font-size: 4rem;
+        background-color: white;
+        padding: 2rem;
+        clip-path: polygon(1rem 1rem, 100% 0%, calc(100% - 1rem) calc(100% - 1rem), 0% 100%);
+    }
+
+    .please-join {
+        color: white;
+        font-size: 2rem;
+        padding: 2rem;
+
+        a {
+            color: white;
+        }
+    }
+</style>
