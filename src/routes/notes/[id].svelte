a3121d18042c8adc4bd48e14544357bafe6ca441
Timothy Foster <tf.auroratide@gmail.com>
2022-01-06 21:10:26 -0600

    Using new api to view note pages now

--- src/routes/notes/[id].svelte
+++ src/routes/notes/[id].svelte
@@ -1,42 +1,41 @@
 <script lang="ts" context="module">
     import type { Load } from '@sveltejs/kit'
-    export const load: Load = async ({ page, fetch }) => {
-        const res = await fetch(`/api/notes/${page.params.id}`)
+    import { NotesApi } from '$lib/notes/api'
 
-        if (res.ok) {
-            return {
-                props: {
-                    note: await res.json(),
-                }
+    export const load: Load = async ({ page, fetch }) => {
+        const api = new NotesApi(fetch)
+        const note = await api.getById(page.params.id)
+
+        return {
+            props: {
+                api,
+                note,
             }
         }
     }
 </script>
 
 <script lang="ts">
-    export let note: {
-        id: string,
-        content: string,
-    }
+    import type { Note } from '$lib/notes/types'
+
+    export let api: NotesApi
+    export let note: Note
 
     let currentContent: string = note.content
 
     const save = () => {
-        fetch(`/api/notes/${note.id}/edits`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                content: currentContent,
-            })
+        return api.edit(note.id, {
+            title: note.title,
+            content: currentContent
         }).then(() => {
             alert('Saved!')
+        }).catch(err => {
+            alert(err)
         })
     }
 </script>
 
-<p>You are looking at note {note.id}.</p>
+<h1>{note.title}</h1>
 <textarea bind:value={currentContent}></textarea>
 <button on:click={save}>Save</button>
-<a href="/notes">Back</a>
+<a href="/me">Back</a>
