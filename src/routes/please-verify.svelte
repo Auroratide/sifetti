3ee58417735a4c97e03e1de6ceebbe7d08033a71
Timothy Foster <tf.auroratide@gmail.com>
2022-01-07 15:13:42 -0600

    Sign up page; just copy-pastaing for now

---
+++ src/routes/please-verify.svelte
@@ -0,0 +1,2 @@
+<h1>Please Verify your email</h1>
+<p>You will be sent a beautiful email. When you click on the link, you will be signed in!</p>
--- src/routes/sign-up.svelte
+++ src/routes/sign-up.svelte
@@ -1,43 +1,88 @@
-<script lang="ts">
-    let email: string
-    let password: string
-    let confirmPassword: string
+<script lang="ts" context="module">
+    import type { Load } from '@sveltejs/kit'
+    import { PeopleApi } from '$lib/people/api'
 
-    const submit = () => {
-        if (password !== confirmPassword) {
-            alert('Uh oh! passwords do not match')
-            return
-        }
-
-        fetch('/api/sign-up', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
+    export const load: Load = async ({ fetch }) => {
+        return {
+            props: {
+                people: new PeopleApi(fetch),
             },
-            body: JSON.stringify({
-                email,
-                password,
-            })
-        }).then(res => {
-            if (res.ok) {
-                return res.json()
-            } else {
-                throw res.json()
-            }
-        }).then(data => {
-            console.log(data)
-        }).catch(err => {
-            console.error(err)
-        })
+        }
     }
 </script>
 
-<form on:submit|preventDefault={submit}>
-    <label for="email">Email:</label>
-    <input id="email" name="email" type="text" bind:value={email} />
-    <label for="password">Password:</label>
-    <input id="password" name="password" type="password" bind:value={password} />
-    <label for="confirm-password">Confirm Password:</label>
-    <input id="confirm-password" name="confirm-password" type="password" bind:value={confirmPassword} />
-    <button type="submit">Sign Up!</button>
-</form>
+<script lang="ts">
+    import { goto } from '$app/navigation'
+
+    export let people: PeopleApi
+
+    let email: string
+    let password: string
+
+    const submit = async () => {
+        try {
+            await people.signUp(email, password)
+            return goto('/please-verify')
+        } catch(err) {
+            console.error(err)
+        }
+    }
+</script>
+
+<svelte:head>
+    <title>Sign Up - Sifetti</title>
+</svelte:head>
+
+<div class="container">
+    <section class="sign-in">
+        <h1>Sign up for Sifetti</h1>
+        <form class="form" on:submit|preventDefault={submit} action={PeopleApi.SIGN_UP} method="post">
+            <label for="email">Email</label>
+            <input required id="email" name="email" type="email" bind:value={email} />
+            <label for="password">Password</label>
+            <input required id="password" name="password" type="password" bind:value={password} />
+            <button type="submit">Sign Up!</button>
+        </form>
+    </section>
+</div>
+
+<style lang="scss">
+    .container {
+        padding: clamp(1rem, 2.5vw, 3rem);
+        background-color: #f7f7ff;
+        height: 100vh;
+    }
+
+    .sign-in {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        background: hsl(210, 68%, 45%);
+        padding: 3rem;
+        max-width: 25rem;
+        margin: auto;
+        clip-path: polygon(1rem 1rem, 100% 0%, calc(100% - 1rem) calc(100% - 1rem), 0% 100%);
+        
+        h1 {
+            color: white;
+            font-size: 1.5rem;
+        }
+    }
+
+    .form {
+        display: flex;
+        flex-direction: column;
+        background-color: white;
+        padding: 2rem;
+
+        label {
+            font-weight: 700;
+            font-size: 1rem;
+        }
+
+        input {
+            margin-bottom: 1rem;
+        }
+    }
+</style>
