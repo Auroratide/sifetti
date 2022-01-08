fd169392474bc8d36445d96cee85c72cea7216b3
Timothy Foster <tf.auroratide@gmail.com>
2022-01-08 11:07:53 -0600

    Some non-serious styling for the note page

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,23 @@
 <slot></slot>
 
-<style global>
+<style lang="scss" global>
     :root {
         --font: 'Kanit', -apple-system, 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Noto Sans', 'Ubuntu', 'Droid Sans', 'Helvetica Neue', sans-serif;
     }
 
     body {
         font-family: var(--font);
+    }
+
+    input[type="text"],
+    input[type="email"],
+    input[type="password"],
+    textarea {
+        border-radius: 0;
+        border: 0.125em solid #e2e2e2;
+
+        &:focus {
+            border-color: hsl(210, 68%, 45%);
+        }
     }
 </style>
--- src/routes/notes/[id].svelte
+++ src/routes/notes/[id].svelte
@@ -36,7 +36,77 @@
     }
 </script>
 
-<input type="text" bind:value={currentTitle} />
-<textarea bind:value={currentContent}></textarea>
-<button on:click={save}>Save</button>
-<a href="/me">Back</a>
+<div class="background">
+    <div class="container">
+        <article class="note" aria-label="{currentTitle}">
+            <div class="input title">
+                <label for="title-input">Title</label>
+                <input id="title-input" type="text" placeholder="Untitled" bind:value={currentTitle} />
+            </div>
+            <div class="input content">
+                <label for="content-input">Content</label>
+                <textarea id="content-input" bind:value={currentContent}></textarea>
+            </div>
+            <button on:click={save}>Save</button>
+            <a href="/me">Back</a>
+        </article>
+    </div>
+</div>
+
+<style lang="scss">
+    .background {
+        background-color: #f7f7ff;
+        padding: 2rem;
+    }
+
+    .container {
+        --angle: 0.5em;
+        background-color: hsl(210, 68%, 45%);
+        padding: 1rem;
+        clip-path: polygon(var(--angle) var(--angle), 100% 0%, calc(100% - var(--angle)) calc(100% - var(--angle)), 0% 100%);
+    }
+
+    .note {
+        background-color: #ffffff;
+        padding: 1rem;
+    }
+
+    .title {
+        font-size: 1.5rem;
+        margin-bottom: 1rem;
+    }
+
+    .content {
+        height: 10rem;
+        margin-bottom: 1rem;
+        
+        textarea {
+            height: 100%;
+        }
+    }
+
+    .input {
+        display: grid;
+        grid-template: 1fr / 1fr;
+        align-items: start;
+
+        label {
+            --angle: 0.1em;
+            grid-column: 1;
+            grid-row: 1;
+            justify-self: start;
+            background-color: hsl(210, 68%, 45%);
+            color: white;
+            font-size: 0.75em;
+            padding: 0 0.5em;
+            line-height: 1;
+            clip-path: polygon(var(--angle) var(--angle), 100% 0%, calc(100% - var(--angle)) calc(100% - var(--angle)), 0% 100%);
+            transform: translate(0.75em, -0.5em);
+        }
+
+        input, textarea {
+            grid-column: 1;
+            grid-row: 1;
+        }
+    }
+</style>
