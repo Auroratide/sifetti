9ac19c6bdde273ce65bc8c416458544c57e77646
Timothy Foster <tf.auroratide@gmail.com>
2022-01-08 11:14:00 -0600

    Markdown test content

--- src/lib/notes/in-memory/notes.ts
+++ src/lib/notes/in-memory/notes.ts
@@ -1,11 +1,18 @@
 import { peopleInMemory } from '../../people/in-memory/people'
 
+const borealis = `
+## Facts
+
+* Aurora Borealis occur in the northern hemisphere
+* The lights are caused by interactions between the solar wind and Earth's magnetosphere
+`
+
 export const notesInMemory = {
     borealis: {
         id: '1',
         author: peopleInMemory.aurora.id,
         title: 'Borealis',
-        content: 'These happen in the northern hemisphere.',
+        content: borealis,
     },
     australis: {
         id: '2',
