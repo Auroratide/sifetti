7db8af35a4d4fa9ec438c1e796ea92affc585888
Timothy Foster <tf.auroratide@gmail.com>
2022-01-12 07:52:08 -0600

    Shuffle css variables around

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,19 +1,11 @@
 <slot></slot>
 
 <style lang="scss" global>
-    :root {
-        --font: 'Poppins', -apple-system, 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Noto Sans', 'Ubuntu', 'Droid Sans', 'Helvetica Neue', sans-serif;
-
-        --skin-bg: hsl(240, 100%, 97%);
-
-        --skin-primary: hsl(210, 68%, 45%);
-        --skin-primary-text: hsl(0, 0%, 100%);
-        
-        --skin-grey: hsl(0, 0%, 89%);
-    }
+    @import './_css/variables';
+    @import './_css/light';
 
     body {
-        font-family: var(--font);
+        font-family: var(--font-reg);
         background-color: var(--skin-bg);
     }
 </style>
