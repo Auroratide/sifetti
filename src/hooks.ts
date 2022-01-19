46c91edf6d8ab3aad7625ded1f6543c29ecb419d
Timothy Foster <tf.auroratide@gmail.com>
2022-01-19 12:45:12 -0600

    No need to fetch the user on literally every request, as this will slow down the server

--- src/hooks.ts
+++ src/hooks.ts
@@ -5,26 +5,28 @@ import { people } from '$lib/beans'
 
 export type Locals = {
     accessToken?: string,
-    person: Person,
 }
 
 export type SessionData = {
-    person: Person,
+    person?: Person,
 }
 
 export const handle: Handle<Locals, unknown> = async ({ request, resolve }) => {
     const cookies = cookie.parse(request.headers.cookie || '')
     if (cookies.access_token) {
         request.locals.accessToken = cookies.access_token
-        const person = await people.getByToken(cookies.access_token)
-        request.locals.person = person
     }
 
     return await resolve(request)
 }
 
 export const getSession: GetSession<Locals, unknown, SessionData> = async (request) => {
+    let person: Person = null
+    if (request.locals.accessToken) {
+        person = await people.getByToken(request.locals.accessToken)
+    }
+
     return {
-        person: request.locals.person,
+        person,
     }
 }
